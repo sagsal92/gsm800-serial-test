a992addb7e654d02731ced5902452ab73f603cd2
Sagar salunkhe <sagsal92@gmail.com>
2018-03-10 17:31:50 +0530

    Update gsmserial.ino

--- gsmserial.ino
+++ gsmserial.ino
@@ -1,5 +1,4 @@
 /*
-  Software serial multple serial test
 
  Receives from the hardware serial, sends to software serial.
  Receives from software serial, sends to hardware serial.
@@ -8,37 +7,21 @@
  * RX is digital pin 2 (connect to TX of other device)
  * TX is digital pin 3 (connect to RX of other device)
 
- Note:
- Not all pins on the Mega and Mega 2560 support change interrupts,
- so only the following can be used for RX:
- 10, 11, 12, 13, 50, 51, 52, 53, 62, 63, 64, 65, 66, 67, 68, 69
-
- Not all pins on the Leonardo and Micro support change interrupts,
- so only the following can be used for RX:
- 8, 9, 10, 11, 14 (MISO), 15 (SCK), 16 (MOSI).
-
- created back in the mists of time
- modified 25 May 2012
- by Tom Igoe
- based on Mikal Hart's example
-
- This example code is in the public domain.
-
  */
 #include <SoftwareSerial.h>
 
 SoftwareSerial gsmSerial(2, 3); // RX, TX
 
-void setup() {
-  // Open serial communications and wait for port to open:
+void setup() 
+{
   Serial.begin(9600);
   Serial.println("hi, serial setup is completed...");
-  // set the data rate for the SoftwareSerial port
   gsmSerial.begin(9600);
   Serial.println("hi, gsm serial setup is completed...");
 }
 
-void loop() { // run over and over
+void loop() 
+{
   if (gsmSerial.available()) {
     Serial.write(gsmSerial.read());
   }
